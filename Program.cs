ac999aed86114da24265956c802dbc281c578649
EckCiting <EckCiting@outlook.com>
2021-12-10 23:56:05 +0800

    Allow users to input their own email

--- Program.cs
+++ Program.cs
@@ -7,14 +7,18 @@ namespace BreakFreeAudioKeyMaker
 		private static void Main(string[] args)
 		{
 			var generatorTuneBlade = new TuneBlade();
-			var resultTuneBlade = generatorTuneBlade.Generate("tuneblade@umetzu.com");
+			var resultTuneBlade = "";
+	                Console.WriteLine("Input your lisence ID (your email) here");
+			while (true){
+				var email = Console.ReadLine();
+				resultTuneBlade = generatorTuneBlade.Generate(email);
+				Console.WriteLine("TuneBlade: " + resultTuneBlade);
+			};
+			
+			//var generatorTuneAero = new TuneAero();
+			//var resultTuneAero = generatorTuneAero.Generate();
 
-			Console.WriteLine("TuneBlade: " + resultTuneBlade);
-
-			var generatorTuneAero = new TuneAero();
-			var resultTuneAero = generatorTuneAero.Generate();
-
-			Console.WriteLine("TuneAero: " + resultTuneAero);
+			//Console.WriteLine("TuneAero: " + resultTuneAero);
 		}
 	}
 }
